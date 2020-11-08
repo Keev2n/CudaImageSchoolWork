e9293b7177b09efa5f5fadf198063f33e51eaf77
keev2n <kewinetu@gmail.com>
2020-11-08 15:11:49 +0100

    added opencv

---
+++ ImageProcessingCuda/ImageProcessingCuda/ImageProcessingCPU.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+}
