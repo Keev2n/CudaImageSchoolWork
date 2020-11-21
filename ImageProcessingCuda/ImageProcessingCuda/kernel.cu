6e28374527eb1e6a93c1651c8dfd33867bed1b2f
keev2n <kewinetu@gmail.com>
2020-11-21 22:17:15 +0100

    Image1 = rgb, Image2 = grayscaled image

--- ImageProcessingCuda/ImageProcessingCuda/kernel.cu
+++ ImageProcessingCuda/ImageProcessingCuda/kernel.cu
@@ -3,45 +3,51 @@
 #include "device_launch_parameters.h"
 #include "ImageProcessing.h"
 #include <math.h>
+#include <iostream>
 
-__global__ void ImageToGrayScale_CUDA(unsigned char* Image, int Channels, int Width, int Height);
 
-void image_toGrayScale_Cuda(unsigned char* Image, int Height, int Width, int Channels) {
+using namespace std;
+
+__global__ void ImageToGrayScale_CUDA(unsigned char* Image, int Row, int Col, int Channels, unsigned char* Image2);
+
+void image_toGrayScale_Cuda(unsigned char* Image, int Row, int Col, int Channels, unsigned char* Image2) {
 	unsigned char* dev_Image = NULL;
+	unsigned char* dev_Image2 = NULL;
 
-	cudaMalloc((void**)&dev_Image, Height * Width * Channels);
+	cudaMalloc((void**)&dev_Image, Row * Col * Channels);
+	cudaMalloc((void**)&dev_Image2, Row * Col);
 
-	cudaMemcpy(dev_Image, Image, Height * Width * Channels, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_Image, Image, Row * Col * Channels, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_Image2, Image2, Row * Col, cudaMemcpyHostToDevice);
 
-	int pixelNumbers = Height * Width;
-	double param, result;
-	param = 1024;
-	result = sqrt(param);
+	int threadNumber = 16;
 
-	int blockNumbers = pixelNumbers / param;
-	int blockXY = sqrt(blockNumbers);
-	dim3 Grid_Image(blockXY, blockXY);
-	dim3 Grid_Image_Thread(result, result);
-	ImageToGrayScale_CUDA <<<Grid_Image, Grid_Image_Thread >> > (dev_Image, Channels, Width, Height);
+	int blockX = (Col / threadNumber) + 1;
+	int blockY = (Row / threadNumber) + 1;
 
-	cudaMemcpy(Image, dev_Image, Height * Width * Channels, cudaMemcpyDeviceToHost);
+	ImageToGrayScale_CUDA <<< dim3(blockX,blockY), dim3(threadNumber, threadNumber) >> > (dev_Image, Row, Col, Channels, dev_Image2);
+	cout << cudaGetLastError() << endl;
+
+	cudaMemcpy(Image, dev_Image, Row * Col * Channels, cudaMemcpyDeviceToHost);
+	cudaMemcpy(Image2, dev_Image2, Row * Col, cudaMemcpyDeviceToHost);
 
 	cudaFree(Image);
+	cudaFree(Image2);
 }
 
-__global__ void ImageToGrayScale_CUDA(unsigned char* Image, int Channels, int Width, int Height) {
+__global__ void ImageToGrayScale_CUDA(unsigned char* Image, int Row, int Col, int Channels, unsigned char* Image2) {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-	if (x < Width && y < Height) {
-		int grayOffset = y * Width + x;
+	if (x < Col && y < Row) {
+		int grayOffset = y * Col + x;
 		int rgbOffset = grayOffset * Channels;
 
-		unsigned char r = Image[rgbOffset];
-		unsigned char g = Image[rgbOffset + 2]; 
-		unsigned char b = Image[rgbOffset + 3];
+		unsigned char b = Image[rgbOffset];
+		unsigned char g = Image[rgbOffset + 1]; 
+		unsigned char r = Image[rgbOffset + 2];
 
-		Image[grayOffset] = 0.21f * r + 0.71f * g + 0.07f * b;
+
+		Image2[grayOffset] = 0.21f * r + 0.71f * g + 0.07f * b;
 	}
-
 }
