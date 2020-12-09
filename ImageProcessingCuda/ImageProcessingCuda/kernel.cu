b53efc8cd722c7e80ec791367e190c457c2d64b6
keev2n <kewinetu@gmail.com>
2020-12-09 19:55:58 +0100

    added sobel's edge finder algorithm

--- ImageProcessingCuda/ImageProcessingCuda/kernel.cu
+++ ImageProcessingCuda/ImageProcessingCuda/kernel.cu
@@ -10,6 +10,8 @@ using namespace std;
 
 __global__ void ImageToGrayScale_CUDA(unsigned char* RGBimage, int Row, int Col, int Channels, unsigned char* GrayImage);
 __global__ void GaussianFilter_CUDA(unsigned char* GrayImage, int Row, int Col, unsigned char* GaussFilteredImage);
+__global__ void SobelEdge_CUDA(unsigned char* gaussImage, unsigned char* sobelEdgeImage, int Col, int Row);
+
 
 void image_toGrayScale_Cuda(unsigned char* Image, int Row, int Col, int Channels, unsigned char* Image2) {
 	unsigned char* dev_Image = NULL;
@@ -90,4 +92,58 @@ __global__ void GaussianFilter_CUDA(unsigned char* GrayImage, int Row, int Col, 
 	unsigned char result = (current * 4 + currentLeft * 2 + currentRight * 2 + currentUpper * 2 + currentUpperLeft
 		+ currentUpperRight + currentLower * 2 + currentLowerLeft + currentLowerRight) / 16;
 	GaussFilteredImage[currentPixelPosition] = result;
+}
+
+__global__ void SobelEdge_CUDA(unsigned char* gaussImage, unsigned char* sobelEdgeImage, int Col, int Row) {
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+	if (x > (Col - 1) || y > (Row - 1)) {
+		return;
+	}
+
+	if (x == 0 || y == 0 || x == (Col - 1) || y == (Row - 1)) {
+		int currentPixelPosition = y * Col + x;
+		sobelEdgeImage[currentPixelPosition] = 0;
+		return;
+	}
+
+	int currentPixelPosition = y * Col + x;
+	int currentLeftPixelPosition = currentPixelPosition - 1;
+	int currentRightPixelPoisiton = currentPixelPosition + 1;
+	int upperPosition = currentPixelPosition - Col;
+	int upperPositionleft = upperPosition - 1;
+	int upperPositionRight = upperPosition + 1;
+	int lowerPosition = currentPixelPosition + Col;
+	int lowerPositonLeft = lowerPosition - 1;
+	int lowerPositionRight = lowerPosition + 1;
+
+	unsigned char current = gaussImage[currentPixelPosition];
+	unsigned char currentLeft = gaussImage[currentLeftPixelPosition];
+	unsigned char currentRight = gaussImage[currentRightPixelPoisiton];
+	unsigned char currentUpper = gaussImage[upperPosition];
+	unsigned char currentUpperLeft = gaussImage[upperPositionleft];
+	unsigned char currentUpperRight = gaussImage[upperPositionRight];
+	unsigned char currentLower = gaussImage[lowerPosition];
+	unsigned char currentLowerLeft = gaussImage[lowerPositonLeft];
+	unsigned char currentLowerRight = gaussImage[lowerPositionRight];
+
+	int GY = (currentUpper * -2) + (currentUpperLeft * -1) + (currentUpperRight * -1)
+		+ (currentLower * 2) + currentLowerLeft + currentLowerRight;
+
+	int GX = (currentLeft * -2) + (currentRight * 2) + (currentUpperLeft * -1)
+		+ (currentUpperRight)+(currentLowerLeft * -1) + (currentLowerRight);
+
+	GX = fabsf(GX);
+	GY = fabsf(GY);
+	int G = GX * GX + GY * GY;
+	G = sqrtf(G);
+
+	if (G >= 50) {
+		sobelEdgeImage[currentPixelPosition] = 100;
+	}
+	else {
+		sobelEdgeImage[currentPixelPosition] = 0;
+	}
+
 }
